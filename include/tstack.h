c86466b82c14aa2aea6440cb9d6e254510d522ec
viktoria shegurova <137227112+shegurovaviktoria@users.noreply.github.com>
2024-04-22 13:02:29 +0300

    tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,46 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    T arr[size];
+    int top;
+public:
+    TStack() : top(-1) {}
+
+    void push(T val) {
+        if (top == size - 1) {
+            throw std::out_of_range("Stack is full");
+        }
+        arr[++top] = val;
+    }
+
+    T pop() {
+        if (top == -1) {
+            throw std::out_of_range("Stack is empty");
+        }
+        return arr[top--];
+    }
+
+    T peek() const {
+        if (top == -1) {
+            throw std::out_of_range("Stack is empty");
+        }
+        return arr[top];
+    }
+
+    bool isEmpty() const {
+        return top == -1;
+    }
+};
+
+int precedence(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    }
+    else if (op == ' ' || op == '/') {
+        return 2;
+    }
+    return 0;
 };
 
 #endif  // INCLUDE_TSTACK_H_
